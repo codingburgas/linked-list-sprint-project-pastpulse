37bd1368bdcd986a954509fb2ce26d40f94630ed
Dimaya Noneva <DHNoneva22@codingburgas.bg>
2025-04-03 00:57:52 +0300

    Add graphical features on the simulator

--- PastPulse/PastPulse/Simulator.cpp
+++ PastPulse/PastPulse/Simulator.cpp
@@ -11,19 +11,22 @@ void simulator()
 
     json j;
     file >> j;
-    srand(time(nullptr)); 
-    int randomIndex = rand() % j.size(); 
+    srand(time(nullptr));
+    int randomIndex = rand() % j.size();
     json selectedRiddle = j[randomIndex];
+    printCentered("  _________      .__                ___________.__             __________.__    .___  .___.__          ", 5);
+    printCentered(" /   _____/ ____ |  |___  __ ____   \\__    ___/|  |__   ____   \\______   \\__| __| _/__| _/|  |   ____  ", 6);
+    printCentered(" \\_____  \\ /  _ \\|  |\\  \\/ // __ \\    |    |   |  |  \_/ __ \\   |       _/  |/ __ |/ __ | |  | _/ __ \\ ", 7);
+    printCentered(" /        (  <_> )  |_\\   /\\  ___/    |    |   |   Y  \\  ___/   |    |   \\  / /_/ / /_/ | |  |_\\  ___/ ", 8);
+    printCentered("/_______  /\____/|____/\\_/  \\___  >   |____|   |___|  /\\___  >  |____|_  /__\\____ \\____ | |____/\\___  >", 9);
+    printCentered("        \\/                      \\/                  \\/     \\/          \\/        \\/    \\/           \\/ ", 10);
     cout << endl << selectedRiddle["introduction"].get<string>() << endl;
 
     list<string> facts;
     for (const auto& fact : selectedRiddle["facts"]) {
         facts.push_back(fact.get<string>());
     }
-    cout << "\nFacts:\n";
-    for (const auto& fact : facts) {
-        cout << "- " << fact << endl;
-    }
+
     string userAnswer;
     bool correct = false;
     list<string> hints;
@@ -41,17 +44,20 @@ void simulator()
         }
     }
     if (!correct) {
-        cout << endl<<"Final chance! Enter your last answer: ";
+        cout << endl << "Final chance! Enter your last answer: ";
         getline(cin, userAnswer);
         if (userAnswer == selectedRiddle["answer"].get<string>()) {
             correct = true;
         }
     }
     if (correct) {
-        cout << "Congratulations! Your answer is correct!"<<endl;
+        cout << "Congratulations! Your answer is correct!" << endl;
     }
     else {
         cout << "Wrong answer. The correct one is: " << selectedRiddle["answer"].get<string>() << endl;
     }
-
+    cout << "\nHere is some additional information about the uprising:\n";
+    for (const auto& fact : facts) {
+        cout << "- " << fact << endl;
+    }
 }

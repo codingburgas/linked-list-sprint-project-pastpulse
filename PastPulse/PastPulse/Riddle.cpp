1719deb322a1ae83bbcda8883893c1e20b1600a0
Dimaya Noneva <DHNoneva22@codingburgas.bg>
2025-04-09 18:20:01 +0300

    Fix logical error

--- PastPulse/PastPulse/Riddle.cpp
+++ PastPulse/PastPulse/Riddle.cpp
@@ -5,6 +5,19 @@
 #include "Colors.h"
 #include "DrawText.h"
 
+string toLower(const string& input) {
+	string result = input;
+	transform(result.begin(), result.end(), result.begin(), ::tolower);
+	return result;
+}
+
+string trim(const string& str) {
+	size_t first = str.find_first_not_of(" \t\n\r\f\v");
+	if (first == string::npos) return "";
+	size_t last = str.find_last_not_of(" \t\n\r\f\v");
+	return str.substr(first, last - first + 1);
+}
+
 bool openFile(ifstream& file, const string& filename) {
 	file.open(filename);
 	if (file.fail()) {
@@ -487,7 +500,10 @@ void displayRiddles(Riddle* head) {
 	cout << "Correct Answer (RIDDLE): ";
 	getline(cin, finalAnswer);
 
-	if (finalAnswer == current->answer) {
+	string userAnswer = trim(toLower(finalAnswer));
+	string correctAnswer = trim(toLower(current->answer));
+
+	if (userAnswer == correctAnswer) {
 		setColor(GREEN, BLACK);
 		cout << "Congratulations, your final answer is correct!" << endl;
 		resetColor();
